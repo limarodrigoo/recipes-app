6bc9855f3e1b9faf8e8be6f25a08027e6b370fe3
ZekaEu <zeka.surf@hotmail.com>
2021-12-09 17:36:08 -0300

    Iniciando Requisito 10 - Header

---
+++ src/pages/ExploreMeal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+
+class ExploreMeal extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header title="Explorar Comidas" showButton={ false } />
+      </div>
+    );
+  }
+}
+
+export default ExploreMeal;
--- src/pages/SearchByArea.js
+++ src/pages/SearchByArea.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 
-class MealPage extends React.Component {
+class SearchByArea extends React.Component {
   render() {
     return (
       <div>
-        <Header title="Comidas" showButton />
+        <Header title="Explorar Origem" showButton />
       </div>
     );
   }
 }
 
-export default MealPage;
+export default SearchByArea;
