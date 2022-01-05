8b434b18f8fb0936735ec8275eeca3e4271c2329
AquilesDantas <aquilesdant@gmail.com>
2022-01-05 16:37:29 -0300

    Requisitos 25 ao 27

--- src/pages/DrinkPage.js
+++ src/pages/DrinkPage.js
@@ -1,14 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 
-class DrinkPage extends React.Component {
-  render() {
+function initialDrinks(drinksData) {
+  const { drinks } = drinksData;
+  const limitDrinks = 12;
+  if (drinks) {
     return (
-      <div>
-        <Header title="Bebidas" showButton />
-      </div>
+      <>
+        {drinks.map((drink, index) => (
+          <div
+            key={ drink.idDrink }
+            data-testid={ `${index}-recipe-card` }
+          >
+            <h3
+              data-testid={ `${index}-card-name` }
+            >
+              {drink.strDrink}
+            </h3>
+            <img
+              data-testid={ `${index}-card-img` }
+              src={ drink.strDrinkThumb }
+              alt=""
+            />
+          </div>
+        )).filter((drinkFilter, index) => index < limitDrinks)}
+      </>
     );
   }
+}
+
+function buttonsCategoriesDrinks(categoriesData) {
+  const limitbuttons = 5;
+  const { drinks } = categoriesData;
+  if (drinks) {
+    return (
+      <>
+        {drinks.map((categorie) => (
+          <button
+            type="button"
+            key={ categorie.strCategory }
+            data-testid={ `${categorie.strCategory}-category-filter` }
+          >
+            { categorie.strCategory }
+          </button>
+        )).filter((categorieFilter, index) => index < limitbuttons)}
+      </>
+    );
+  }
+}
+
+function DrinkPage() {
+  const [drinksData, setdrinksData] = useState([]);
+  const [categoriesDrinksData, setCategoriesDrinksData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const drinksResponse = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+      const drinkData = await drinksResponse.json();
+      setdrinksData(drinkData);
+      const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
+      const categorieData = await response.json();
+      setCategoriesDrinksData(categorieData);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <Header title="Bebidas" showButton />
+      {buttonsCategoriesDrinks(categoriesDrinksData)}
+      {initialDrinks(drinksData)}
+    </div>
+  );
 }
 
 export default DrinkPage;
--- src/pages/MealPage.js
+++ src/pages/MealPage.js
@@ -1,14 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 
-class MealPage extends React.Component {
-  render() {
+function initialMeals(mealsData) {
+  const { meals } = mealsData;
+  const limitDrinks = 12;
+
+  if (meals) {
     return (
-      <div>
-        <Header title="Comidas" showButton />
-      </div>
+      <>
+        {meals.map((meal, index) => (
+          <div
+            key={ meal.idMeal }
+            data-testid={ `${index}-recipe-card` }
+          >
+            <h3
+              data-testid={ `${index}-card-name` }
+            >
+              {meal.strMeal}
+            </h3>
+            <img
+              data-testid={ `${index}-card-img` }
+              src={ meal.strMealThumb }
+              alt=""
+            />
+          </div>
+        )).filter((mealFilter, index) => index < limitDrinks)}
+      </>
     );
   }
+}
+
+function buttonsCategoriesDrinks(categoriesData) {
+  const limitbuttons = 5;
+  const { meals } = categoriesData;
+  if (meals) {
+    return (
+      <>
+        {meals.map((categorie) => (
+          <button
+            type="button"
+            key={ categorie.strCategory }
+            data-testid={ `${categorie.strCategory}-category-filter` }
+          >
+            { categorie.strCategory }
+          </button>
+        )).filter((categorieFilter, index) => index < limitbuttons)}
+      </>
+    );
+  }
+}
+
+function MealPage() {
+  const [mealsData, setmealsData] = useState([]);
+  const [categoriesMealsData, setCategoriesMealsData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const mealResponse = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+      const mealData = await mealResponse.json();
+      setmealsData(mealData);
+      const categorieResponse = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+      const categorieData = await categorieResponse.json();
+      setCategoriesMealsData(categorieData);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <Header title="Comidas" showButton />
+      {buttonsCategoriesDrinks(categoriesMealsData)}
+      {initialMeals(mealsData)}
+    </div>
+  );
 }
 
 export default MealPage;
