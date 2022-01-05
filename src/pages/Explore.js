3cd8c0d59b5105119c31c4e0f4f0b732f4c9a178
Cássia Elen <cassiahherzog@cassiahherzog.com>
2022-01-05 20:40:58 -0300

    Tela Explorar

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 
-class Explore extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header title="Explorar" showButton={ false } />
-      </div>
-    );
-  }
+function Explore() {
+  const history = useHistory();
+  const goDrink = () => history.push('/explorar/bebidas');
+  const goMeal = () => history.push('/explorar/comidas');
+
+  return (
+    <>
+      <Header />
+      <section>
+        <button
+          type="button"
+          name="Explorar Comidas"
+          data-testid="explore-food"
+          onClick={ goMeal }
+        >
+          Explorar Comidas
+        </button>
+        <button
+          type="button"
+          name="Explorar Bebidas"
+          data-testid="explore-drinks"
+          onClick={ goDrink }
+        >
+          Explorar Bebidas
+        </button>
+      </section>
+    </>
+  );
 }
 
 export default Explore;
