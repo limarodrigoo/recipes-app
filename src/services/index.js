9b4013fcfbc9333683ea9db91f342625fd67fe66
Xafixav <xafiworld@gmail.com>
2022-01-12 16:31:35 -0300

    created components filterButtons and service that filter Obj

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,8 @@
-const treatsRecipeDone = () => JSON.parse(localStorage.getItem('doneRecipes'));
+export const treatsRecipeDone = () => JSON.parse(localStorage.getItem('doneRecipes'));
 
-export default treatsRecipeDone;
+export const arrRecipesFilter = (arrOfObj, filterChosen) => {
+  let result = arrOfObj;
+  if (filterChosen !== 'all') result = arrOfObj.filter((el) => el.type === filterText);
+
+  return result;
+};
