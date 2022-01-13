900fa2fdedc250f436627772141f8cd244a7f50c
ZekaEu <zeka.surf@hotmail.com>
2022-01-13 08:17:38 -0300

    Tela de Perfil (Esqueci de criar outra branch)

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,14 +1,46 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 
-class ProfilePage extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header title="Perfil" showButton={ false } />
-      </div>
-    );
+function ProfilePage () {
+  const { email } = JSON.parse(localStorage.getItem('user'));
+  const history = useHistory();
+  const handleExitBtn = () => {
+    localStorage.clear();
+    history.push('/');
   }
-}
+
+  return (
+    <>
+      <Header title="Perfil" showButton={ false } />
+      <div>
+        <p data-testid="profile-email">{ email }</p>
+        <div>
+          <button
+            onClick={ () => history.push('/receitas-feitas') }
+            type="button"
+            data-testid="profile-done-btn"
+          >
+            Receitas Feitas
+          </button>
+          <button
+            onClick={ () => history.push('/receitas-favoritas') }
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Receitas Favoritas
+          </button>
+          <button
+            onClick={ handleExitBtn }
+            type="button"
+            data-testid="profile-logout-btn"
+          >
+            Sair
+          </button>
+        </div>
+      </div>
+    </>
+  );
+  }
 
 export default ProfilePage;
